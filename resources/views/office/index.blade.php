ffedbfa9382ca4da9cf35d49aee3e8ad0a1e434b
ScreamShadows <sam021195@hotmail.com>
2022-03-01 11:37:43 -0600

    update sucursal

--- resources/views/office/index.blade.php
+++ resources/views/office/index.blade.php
@@ -27,7 +27,7 @@
                                         <tr>
                                             <td style="display: none;">{{ $sucursale->id}}</td>
                                             <td>{{ $sucursale->name }}</td>
-                                            <td>{{ $sucursale->number }}</td>
+                                            <td>{{ $sucursale->phone }}</td>
                                             <td>{{ $sucursale->responsable }}</td>
                                             <td>{{ $sucursale->address->street }} {{ $sucursale->address->number }} {{ $sucursale->address->suburb }}, {{ $sucursale->address->postal_code }}, {{ $sucursale->address->city }}, {{ $sucursale->address->state }}, {{ $sucursale->address->country }}</td>
                                             <td>{{ $sucursale->business->name }}</td>
