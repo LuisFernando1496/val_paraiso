35b0c1f3874337550911ece5ee4b9b85f5126b77
ScreamShadows <sam021195@hotmail.com>
2022-03-01 13:23:26 -0600

    index user problems

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Usuarios</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="{{ route('usuarios.create') }}" class="btn btn-warning">Nuevo</a>
+                            <table class="table table-striped mt-2">
+                                <thead style="background-color: #6777ef;">
+                                    <th style="display: none;">ID</th>
+                                    <th style="color: #fff;">Nombre(s)</th>
+                                    <th style="color: #fff;">Apellido Paterno</th>
+                                    <th style="color: #fff;">Apellido Materno</th>
+                                    <th style="color: #fff;">Correo</th>
+                                    <th style="color: #fff;">Sucursal</th>
+                                    <th style="color: #fff;">Rol</th>
+                                    <th style="color: #fff;">Editar</th>
+                                    <th style="color: #fff;">Eliminar</th>
+                                </thead>
+                                <tbody>
+                                    @forelse ($users as $user)
+                                        <tr>
+                                            <td style="display: none;">{{ $user->id }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->last_name }}</td>
+                                            <td>{{ $user->second_last_name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->office->name ?? '' }}</td>
+                                            <td>
+                                                @if (!empty($user->getRoleNames()))
+                                                    @foreach ($user->getRoleNames as $rolName)
+                                                        <h5><span class="badge badge-dark">{{ $rolName }}</span></h5>
+                                                    @endforeach
+                                                @endif
+                                            </td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    @empty
+
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
