b9bdd2a62aab7e7a2e80efdaa2b24a0a441b158f
ScreamShadows <sam021195@hotmail.com>
2022-03-06 22:54:56 -0600

    ya actualiza solo la tabla

--- resources/views/vender/vender.blade.php
+++ resources/views/vender/vender.blade.php
@@ -129,7 +129,65 @@
                                                         <th style="color: #fff">Total</th>
                                                         <th style="color: #fff">Eliminar</th>
                                                     </thead>
-                                                    @include('vender.carrito',$carrito)
+                                                    <tbody id="cuerpo">
+                                                        @forelse ($carrito as $item)
+                                                            @if ($item->service_id != null)
+                                                                @php
+                                                                    $total = $item->service->price * $item->quantity;
+                                                                    $gtotal += $total;
+                                                                @endphp
+                                                                <tr>
+                                                                    <td>{{ $item->service->name }}</td>
+                                                                    <td>
+                                                                        ${{ $item->service->price }}
+                                                                        <input type="number" name="" id="precio{{$item->id}}" hidden value="{{$item->service->price}}">
+                                                                    </td>
+                                                                    <td>Servicio</td>
+                                                                    <td>
+                                                                        {!! Form::number('quantity', $item->quantity, array('class' => 'form-control quantity','id' => 'quantity'.$item->id,'data-id' => $item->id, 'onChange' => 'cambiocantidad()')) !!}
+                                                                    </td>
+                                                                    <td>
+                                                                        {!! Form::number('percent', $item->percent, array('class' => 'form-control percent','id' => 'percent'.$item->id,'data-id' => $item->id)) !!}
+                                                                    </td>
+                                                                    <td>
+                                                                        {!! Form::number('total', $total, array('class' => 'form-control','id' => 'total'.$item->id)) !!}
+                                                                    </td>
+                                                                    <td>
+                                                                        <button class="btn btn-danger">Quitar</button>
+                                                                    </td>
+                                                                </tr>
+                                                            @else
+                                                                @php
+                                                                    $total = $item->costprice->price * $item->quantity;
+                                                                    $gtotal += $total;
+                                                                @endphp
+                                                                <tr>
+                                                                    <td>{{ $item->costprice->vendorproduct->product->name }}</td>
+                                                                    <td>
+                                                                        ${{ $item->costprice->price }}
+                                                                        <input type="number" name="" id="precio{{$item->id}}" hidden value="{{$item->costprice->price}}">
+                                                                    </td>
+                                                                    <td>{{ $item->costprice->vendorproduct->product->mark }}</td>
+                                                                    <td>
+                                                                        {!! Form::number('quantity', $item->quantity, array('class' => 'form-control quantity','id' => 'quantity'.$item->id,'data-id' => $item->id)) !!}
+                                                                    </td>
+                                                                    <td>
+                                                                        {!! Form::number('percent', $item->percent, array('class' => 'form-control percent','id' => 'percent'.$item->id,'data-id' => $item->id)) !!}
+                                                                    </td>
+                                                                    <td>
+                                                                        {!! Form::number('total', $total, array('class' => 'form-control','id' => 'total'.$item->id)) !!}
+                                                                    </td>
+                                                                    <td>
+                                                                        <button class="btn btn-danger">Quitar</button>
+                                                                    </td>
+                                                                </tr>
+                                                            @endif
+                                                        @empty
+                                                            <tr>
+                                                                <td colspan="7">Sin registros</td>
+                                                            </tr>
+                                                        @endforelse
+                                                    </tbody>
                                                 </table>
                                             </div>
                                         </div>
@@ -273,7 +331,7 @@
                     console.log(response);
                     status = response['status'];
                     if (status == 200) {
-                        $("#cuerpo").load("#cuerpo");
+                        $("#refresh").load(" #refresh");
                     }
                 });
             });
@@ -291,12 +349,13 @@
                     console.log(response);
                     status = response['status'];
                     if (status == 200) {
-                        $("#cuerpo").load("#cuerpo");
+                        $("#refresh").load(" #refresh");
                     }
                 });
             });
 
             $('.quantity').on('change',function(){
+                alert('Cambio');
                 var valor = $(this).val();
                 var id = $(this).data('id');
                 var precio = $('#precio'+id).val();
@@ -317,8 +376,7 @@
 
                     }
                     else {
-                        //location.reload();
-                        //$("#tablarefresh").load("#tablarefresh");
+                        $("#refresh").load(" #refresh");
                     }
                 });
             });
@@ -334,6 +392,33 @@
                 $('#subtotal').val(total);
                 $('#total').val(total);
             });
+
+            function cambiocantidad() {
+                alert('Cambio');
+                var valor = $(this).val();
+                var id = $(this).data('id');
+                var precio = $('#precio'+id).val();
+                var total = precio * valor;
+                $('#total'+id).val(total);
+                $('#subtotal').val(total);
+                $('#total').val(total);
+                var data = {
+                    quantity : valor,
+                };
+                $.ajax({
+                    type: "PUT",
+                    url: "/vender/"+id,
+                    data: data,
+                }).then(function(data){
+                    var status = data['status'];
+                    if (status != 200) {
+
+                    }
+                    else {
+                        $("#refresh").load(" #refresh");
+                    }
+                });
+            }
         });
     </script>
 @endsection
